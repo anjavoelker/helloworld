6c95180b76aa6849bb0b7e4dceedf8b429e2e16f
anjavoelker <anvoelker@googlemail.com>
2015-08-05 21:49:57 +0200

    Final

---
+++ fahrenheit.py
@@ -0,0 +1,21 @@
+
+# Schritt 1: Ausgabe fuer den Nutzer
+print "Bitte geben Sie die Temperatur in Fahrenheit ein: "
+# Schritt 2: Nutzer gibt Fahrenheit ein
+fahrenheit = raw_input()
+# Schritt 3: celsius ausrechnen
+celsius = int(5.0/9 * (int(fahrenheit) -32))
+# Schritt 4 Ausgabe fuer den Nutzer
+print fahrenheit, "Grad Fahrenheit sind", celsius, "Grad Celsius"
+
+
+
+
+
+
+
+
+
+
+
+		
